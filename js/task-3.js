6a341923450f650ba0a4900cec2fb6f75eca5da9
Royalbud <3607350@gmail.com>
2025-12-16 18:10:42 +0200

    remove double of task-3

--- js/task-3.js
+++ js/task-3.js
@@ -5,14 +5,3 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-
-// Like task-1 & task-2
-function getElementWidth2(content2, padding2, border2) {
-    const totalWidth2 = Number.parseFloat(content2) + 2 * Number.parseFloat(padding2) + 2 * Number.parseFloat(border2);
-    return `Total width of your object is ${totalWidth2}px`;
-};
-
-console.log(getElementWidth2("56px", "18px", "12.5px")); // 74
-console.log(getElementWidth2("60px", "22px", "8.5px")); // 101
-console.log(getElementWidth2("420px", "3.5px", "6px")); // 200
